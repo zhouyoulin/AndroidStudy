e94fda9b5ab27c585ec90f55459711091d580ee2
zhouyoulin <779606669@qq.com>
2020-12-10 19:24:26 +0800

    modify - okhttp 加入

--- MyApplication/app/src/main/java/com/example/myapplication/main/shanghai/view/ShanghaiDetailActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/main/shanghai/view/ShanghaiDetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v4.view.ViewCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,12 +15,23 @@ import com.example.myapplication.R;
 import com.example.myapplication.base.BaseActivity;
 import com.example.myapplication.base.ViewInject;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
 import butterknife.BindView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @ViewInject(mainLayoutId = R.layout.activity_shanghai_detail)
 public class ShanghaiDetailActivity extends BaseActivity {
 
     private static String mShanghaiDetailActivityTag = "ShanghaiDetailActivity";
+
+    private static final String TAG = "ShanghaiDetailActivity";
 
     @BindView(R.id.iv_shanghai_detail)
     ImageView ivShanghaiDetail;
@@ -27,6 +39,24 @@ public class ShanghaiDetailActivity extends BaseActivity {
     @Override
     protected void afterBindView() {
         initAnim();
+        initGetData();
+    }
+
+    private void initGetData() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "onFailure: " + e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "onResponse: "+response.body().string());
+            }
+        });
     }
 
     private void initAnim() {
